508051aca9aae68ff2f9862d7db8b5a367f1070c
Rafael Krämer da Silva <114179855+RafaelGaiteiro@users.noreply.github.com>
2022-09-22 16:31:52 -0300

    .

--- TicTacToe_e/js/arquivo.js
+++ TicTacToe_e/js/arquivo.js
@@ -81,3 +81,53 @@ function handleRestartGame() {
 }
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+
+
+/*
+
+= Operador de Atribuição
+    É usado para atribuir valores a uma variável.
+    Exemplo:
+
+    x = 10
+    y = 20
+    z = x + y
+ 
+
+== Igual a ou Equal to
+    É usado para comparação entre duas variáveis, independentemente do tipo de dados da variável.
+    Exemplo:
+
+    x = 10;
+
+    x == 8       ->  retorna false
+    x == 10      ->  retorna true
+
+    No caso abaixo o valor "10" é uma String.
+    x == "10"    ->  retorna true
+
+ 
+
+=== Valor e Tipo igual
+    É usado para comparação entre duas variáveis, mas isso irá verificar o tipo estrito, o que significa que ele irá verificar o tipo de dados e comparar dois valores.
+    
+    Para x= 10 temos que :
+    x === 8    ->   retorna false
+    x === 10   ->   retorna true
+    x === "10" ->   retorna false
+
+Assim o operador == compara por 'resultado'; Ele converte o que você quer comparar e verifica.
+
+Ja o operador ===  compara o valor e o tipo, (quando você usa o operador === você força a comparar o tipo também) então só vai ser true se for exatamente igual (valor e tipo).
+
+Assim temos:
+
+if (true == 'true')  	// Retorna true
+if (true == '1')       	// Retorna  true
+if (true == true)    	// Retorna  true
+if (true === 'true') 	// Retorna  false
+if (true === '1')    	// Retorna  false
+if (true === true)   	// Retorna  true (igualdade estrita, mesmo tipo e mesmo valor)
+
+*/
